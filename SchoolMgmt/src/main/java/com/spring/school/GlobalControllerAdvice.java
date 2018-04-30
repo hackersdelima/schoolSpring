c8d6145d4e7a538575e3a5ab8f4717757100467a
shishir <shishir4913120@gmail.com>
2018-04-30 09:46:39 +0545

    do not go here 4.2 update

--- SchoolMgmt/src/main/java/com/spring/school/GlobalControllerAdvice.java
+++ SchoolMgmt/src/main/java/com/spring/school/GlobalControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.spring.school;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice(annotations=Controller.class)
 public class GlobalControllerAdvice {
