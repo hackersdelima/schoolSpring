862fe328ac0046524b08b04b42f7fff053b9fe74
sunil <suniltandukar50@gmail.com>
2019-03-31 11:35:03 +0545

    target deleted

--- target/classes/snippet/Snippet.java
+++
@@ -1,6 +0,0 @@
-package snippet;
-
-public class Snippet {
-	SELECT studentdetail.studentid, studentdetail.studentname,studentdetail.rollno from studentdetail  join optcoursetbl on studentdetail.studentid=optcoursetbl.studentid  join subjectlist on optcoursetbl.subjectid=subjectlist.subjectid where subjectlist.subjectid='105' and studentdetail.admissionclass='Class 11 HUM' and studentdetail.section='Section B'
-}
-
